ee46d77804336286f6648472ff2c26a9e55de17e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-15 18:47:19 +0200

    setup mongoose connection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
+require('dotenv').config();
 
 const app = express()
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URL
 
 app.listen(5000, () => console.log('Server started on port 5000'))
