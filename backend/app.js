1cbaa24a9016538ca6c313411672962bd2bc8fe4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-16 12:39:47 +0200

    add body parser

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const api = require('./api')
 
 const app = express()
+
+app.use(bodyParser.json());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
